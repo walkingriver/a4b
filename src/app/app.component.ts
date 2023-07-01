2c85d0206bbde05b6cf226648103aa1b0c223954
Michael Callaghan <mikecallaghan67@gmail.com>
2023-07-01 18:22:07 -0400

    Chapter 4 - before potential optimizations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LoadingComponent } from './loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, LoadingComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Angular-for-Business';
+  isLoading = false;
+
+  load() {
+    this.isLoading = true;
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 3000);
+  }
 }
